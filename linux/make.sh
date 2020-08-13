5c18bbb17fcc1138172529743dff8e6279dce48d
calebbuahin <7217571+cbuahin@users.noreply.github.com>
2020-08-13 07:30:53 -0400

    Added build options to linux and darwin

--- linux/make.sh
+++ linux/make.sh
@@ -22,6 +22,7 @@ if [[ ! -v PROJECT ]]; then echo "ERROR: PROJECT must be defined"; exit 1; fi
 export BUILD_HOME="build"
 
 GENERATOR="Ninja"
+TESTING=0
 
 
 # determine project directory
@@ -32,12 +33,21 @@ cd ${SCRIPT_HOME}/../../
 
 # perform the build
 cmake -E make_directory ${BUILD_HOME}
-cmake -E chdir ${BUILD_HOME} cmake -G ${GENERATOR} ..
-cmake --build ./${BUILD_HOME} --config Release --target all -- -v
 
+if [ ${TESTING}=1 ]; then
+# cmake -E chdir ${BUILD_HOME} cmake -G ${GENERATOR} ..
+# cmake --build ./${BUILD_HOME} --config Release --target all -- -v
+    cmake -E chdir ./${BUILD_HOME} cmake -G ${GENERATOR} -DBUILD_TESTS=ON .. \
+    && cmake --build ./${BUILD_HOME}  --config Debug \
+    & echo. && cmake -E chdir ./${BUILD_HOME}  ctest -C Debug --output-on-failure
+
+else
+    cmake -E chdir ./${BUILD_HOME} cmake -G ${GENERATOR} -DBUILD_TESTS=OFF .. \
+    && cmake --build ./${BUILD_HOME} --config Release --target package \
+    && cp ./${BUILD_HOME}/*.zip ./upload >&1
+fi
 
 export PLATFORM="Linux"
-
 
 # return user to current dir
 cd ${CUR_DIR}
