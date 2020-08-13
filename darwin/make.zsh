3e37be55a5bbd4b759f04514ea369289a44e7d1f
calebbuahin <7217571+cbuahin@users.noreply.github.com>
2020-08-13 12:42:52 -0400

    Added dawrin config to accept generator and testing args

--- darwin/make.zsh
+++ darwin/make.zsh
@@ -14,25 +14,80 @@
 #
 #  Environment Variables:
 #    PROJECT
+#  
+#  Optional Arguments:
+#    -g ("GENERATOR") defaults to "Ninja"
+#    -t builds and runs unit tests (requires Boost)
+
 
 # Check to make sure PROJECT is defined
 [[ ! -v PROJECT ]] && { echo "ERROR: PROJECT must be defined"; return 1 }
 
 export BUILD_HOME="build"
 
-GENERATOR="Ninja"
-
-
 # determine project directory
 CUR_DIR=${PWD}
 SCRIPT_HOME=${0:a:h}
 cd ${SCRIPT_HOME}/../../
 
+# Check to make sure PROJECT is defined
+[[ ! -v PROJECT ]] && { echo "ERROR: PROJECT must be defined"; return 1 }
+
+echo INFO: Building ${PROJECT}  ...
+
+GENERATOR="Ninja"
+TESTING=0
+
+POSITIONAL=()
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -g|--gen)
+    GENERATOR="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -t|--test)
+    TESTING="$2"
+    echo ${TESTING}
+    shift # past argument
+    shift # past value
+    ;;
+    --default)
+    DEFAULT=YES
+    shift # past argument
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+
+set -- "${POSITIONAL[@]}" # restore positional parameters
 
 # perform the build
 cmake -E make_directory ${BUILD_HOME}
-cmake -E chdir ${BUILD_HOME} cmake -G ${GENERATOR} ..
-cmake --build ./${BUILD_HOME} --config Release --target all -- -v
+
+if [ ${TESTING}=1 ]; 
+then
+# cmake -E chdir ${BUILD_HOME} cmake -G ${GENERATOR} ..
+# cmake --build ./${BUILD_HOME} --config Release --target all -- -v
+    cmake -E chdir ./${BUILD_HOME} cmake -G ${GENERATOR} -DBUILD_TESTS=ON .. \
+    && cmake --build ./${BUILD_HOME}  --config Debug \
+    && cmake -E chdir ./${BUILD_HOME}  ctest -C Debug --output-on-failure
+
+else
+    cmake -E chdir ./${BUILD_HOME} cmake -G ${GENERATOR} -DBUILD_TESTS=OFF .. \
+    && cmake --build ./${BUILD_HOME} --config Release --target package \
+    && cp ./${BUILD_HOME}/*.zip ./upload >&1
+fi
+
+# cmake -E chdir ${BUILD_HOME} cmake -G ${GENERATOR} ..
+# cmake --build ./${BUILD_HOME} --config Release --target all -- -v
 
 
 export PLATFORM="Darwin"
