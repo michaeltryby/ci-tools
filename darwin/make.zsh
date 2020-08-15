a80aafc26d79537e6935e17786c663c4b365b3de
calebbuahin <7217571+cbuahin@users.noreply.github.com>
2020-08-15 00:03:59 -0400

    Fixed generator parsing error

--- darwin/make.zsh
+++ darwin/make.zsh
@@ -47,7 +47,6 @@ key="$1"
 case $key in
     -g|--gen)
     GENERATOR="$2"
-    echo generator ${GENERATOR}
     shift # past argument
     shift # past value
     ;;
@@ -70,7 +69,7 @@ if [ ${TESTING}=1 ];
 then
 # cmake -E chdir ${BUILD_HOME} cmake -G ${GENERATOR} ..
 # cmake --build ./${BUILD_HOME} --config Release --target all -- -v
-    cmake -E chdir ./${BUILD_HOME} cmake -G ${GENERATOR} -DBUILD_TESTS=ON .. \
+    cmake -E chdir ./${BUILD_HOME} cmake -G "${GENERATOR}" -DBUILD_TESTS=ON .. \
     && cmake --build ./${BUILD_HOME}  --config Debug \
     && cmake -E chdir ./${BUILD_HOME}  ctest -C Debug --output-on-failure
 
--- linux/make.sh
+++ linux/make.sh
@@ -49,7 +49,6 @@ key="$1"
 case $key in
     -g|--gen)
     GENERATOR="$2"
-    echo generator ${GENERATOR}
     shift # past argument
     shift # past value
     ;;
@@ -65,7 +64,6 @@ done
 
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
-
 # perform the build
 cmake -E make_directory ${BUILD_HOME}
 
@@ -73,7 +71,7 @@ if [ ${TESTING}=1 ];
 then
 # cmake -E chdir ${BUILD_HOME} cmake -G ${GENERATOR} ..
 # cmake --build ./${BUILD_HOME} --config Release --target all -- -v
-    cmake -E chdir ./${BUILD_HOME} cmake -G ${GENERATOR} -DBUILD_TESTS=ON .. \
+    cmake -E chdir ./${BUILD_HOME} cmake -G "${GENERATOR}" -DBUILD_TESTS=ON .. \
     && cmake --build ./${BUILD_HOME}  --config Debug \
     && cmake -E chdir ./${BUILD_HOME}  ctest -C Debug --output-on-failure
 
