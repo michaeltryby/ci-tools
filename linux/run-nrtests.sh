f54c6ab9004b45316cd5f7394f2bac821ec50cdb
calebbuahin <7217571+cbuahin@users.noreply.github.com>
2020-08-27 21:45:20 -0400

    Fixed checking regression testing return code

--- linux/run-nrtests.sh
+++ linux/run-nrtests.sh
@@ -94,12 +94,12 @@ cd ./benchmark
 
 if [ $? -eq 0 ]
 then
+    echo "INFO: nrtest compare exited successfully"
+    mv receipt.json ${PROJ_DIR}/upload/receipt.json
+else
     echo ERROR: nrtest exited with errors
     tar -zcvf benchmark-${PLATFORM}.tar.gz ./${PROJECT}-${SUT_BUILD_ID}
     mv benchmark-${PLATFORM}.tar.gz ${PROJ_DIR}/upload/benchmark-${PLATFORM}.tar.gz
-else
-    echo "INFO: nrtest compare exited successfully"
-    mv receipt.json ./${PROJ_DIR}/upload/receipt.json
 fi
 
 # return user to current dir
